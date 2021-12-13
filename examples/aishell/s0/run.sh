b1d4b76ca38aabefe842388cf1b7392c0460722d
InfluencerNGZK <jasonxing_380a@foxmail.com>
2021-12-13 10:05:30 +0800

    modify personal configurations

--- examples/aishell/s0/run.sh
+++ examples/aishell/s0/run.sh
@@ -5,12 +5,12 @@
 
 # Use this to control how many gpu you use, It's 1-gpu training if you specify
 # just 1gpu, otherwise it's is multiple gpu training based on DDP in pytorch
-export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+export CUDA_VISIBLE_DEVICES="1"
 # The NCCL_SOCKET_IFNAME variable specifies which IP interface to use for nccl
 # communication. More details can be found in
 # https://docs.nvidia.com/deeplearning/nccl/user-guide/docs/env.html
 # export NCCL_SOCKET_IFNAME=ens4f1
-export NCCL_DEBUG=INFO
+# export NCCL_DEBUG=INFO
 stage=0 # start from 0 if you need to start from data preparation
 stop_stage=6
 # The num of nodes or machines used for multi-machine training
@@ -22,7 +22,7 @@ num_nodes=1
 # the third one set node_rank 2, and so on. Default 0
 node_rank=0
 # data
-data=/export/data/asr-data/OpenSLR/33/
+data=/export
 data_url=www.openslr.org/resources/33
 
 nj=16
@@ -53,7 +53,7 @@ decode_modes="ctc_greedy_search ctc_prefix_beam_search attention attention_resco
 
 if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
     echo "stage -1: Data Download"
-    local/download_and_untar.sh ${data} ${data_url} data_aishell
+    # local/download_and_untar.sh ${data} ${data_url} data_aishell
     local/download_and_untar.sh ${data} ${data_url} resource_aishell
 fi
 
